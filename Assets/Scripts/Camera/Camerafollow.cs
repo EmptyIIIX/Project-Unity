da465b24ecd4b5858531489241d526ac3fe4f459
Thaizan666666 <Thaizan246810@gmail.com>
2025-12-24 21:55:43 +0700

    Update Camera
    Camera can follows player

---
+++ Assets/Scripts/Camera/Camerafollow.cs
@@ -0,0 +1,34 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Camerafollow : MonoBehaviour
+{
+    private Vector3 offset = new Vector3(0f, 0f, -1f);
+    private float smoothTime = 0.25f;
+    private Vector3 velocity = Vector3.zero;
+    Vector3 targetPosition;
+    public bool Active = false;
+
+    [SerializeField] private Transform target;
+    [SerializeField] private Transform roomtarget;
+
+    private void Update()
+    {
+        if (!Active)
+        {
+            targetPosition = target.position + offset;
+        }
+        else
+        {
+            targetPosition = new Vector3((roomtarget.position.x / 2f),(roomtarget.position.y / 2f),(-1f));
+        }
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("roomActive"))
+        {
+            Active = true;
+        }
+    }
+}
