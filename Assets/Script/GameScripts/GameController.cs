1e6ebb0acaf51a1c1cda01c7cb14389bf4e88268
Thaizan666666 <Thaizan246810@gmail.com>
2026-03-16 19:25:29 +0700

    Fix Update

--- Assets/Script/GameScripts/GameController.cs
+++ Assets/Script/GameScripts/GameController.cs
@@ -1,107 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.LowLevel;
-//using UnityEngine.Rendering.Universal;
-//using UnityEngine.UI;
-
-//public class GameController : MonoBehaviour
-//{
-//    int progressSoulAmount;
-//    public Slider progressSoulSlider;
-
-//    public GameObject player;
-//    public GameObject LoadCanvas;
-//    public List<GameObject> levels;
-//    public Transform SpawnPoint;
-//    private int currentLevelIndex = 0;
-
-//    public GameObject gameOverScreen;
-//    public static event Action OnReset;
-
-//    // Start is called once before the first execution of Update after the MonoBehaviour is created
-//    void Start()
-//    {
-//        progressSoulAmount = 0;
-//        progressSoulSlider.value = 0;
-//        Soul.OnSoulCollect += IncreaseProgressSoulAmount;
-//        //HoldToLoadLevel.OnHoldComplete += LoadNextLevel;          have to go into the gate
-//        HoldToLoadLevel.OnHoldComplete += LoadHealPlayer;
-//        PlayerAttack.HitEnemy += absorbSoul;
-//        LoadCanvas.SetActive(false);
-//        PlayerHealth2.OnPlayerDied += GameOverScreen;
-//        gameOverScreen.SetActive(false);
-
-//        GateToNextLevel.IntoGate += LoadNextLevel;
-//        SpawnPoint = GetComponent<Transform>();
-//    }
-
-//    void GameOverScreen()
-//    {
-//        gameOverScreen.SetActive(true);
-
-//        Time.timeScale = 0;     //Time stop###
-//    }
-
-//    public void ResetGame()
-//    {
-//        gameOverScreen.SetActive(false);
-//        LoadLevel(0);       //this can use with teleport pole*****************************
-//        OnReset.Invoke();
-
-//        if (CheckpointManager.Instance != null)
-//            CheckpointManager.Instance.ResetCheckpoint();
-
-//        Time.timeScale = 1;     //Time run###
-//    }
-//    void IncreaseProgressSoulAmount(int amount)
-//    {
-//        progressSoulAmount += amount;
-//        progressSoulSlider.value = progressSoulAmount;
-//        if (progressSoulAmount >= 100)
-//        {
-//            //Collect Complete
-//            Debug.Log("Level Complete");
-
-//            //this can change to be onGate to boss room and not this method
-//            LoadCanvas.SetActive(true);
-//        }
-//    }
-
-//    void LoadLevel(int level)
-//    {
-//        LoadCanvas.SetActive(false);
-
-//        levels[currentLevelIndex].gameObject.SetActive(false);
-//        levels[level].gameObject.SetActive(true);
-//        currentLevelIndex = level;
-        
-//        //if (player != null && CheckpointManager.Instance != null)
-//        //player.transform.position = CheckpointManager.Instance.GetRespawnPoint(levels[level].transform.position);
-//        player.transform.position = SpawnPoint.transform.position;
-//        Debug.Log("Player is actived");
-//    }
-
-//    void LoadNextLevel()
-//    {
-//        int nextLevelIndex = (currentLevelIndex == levels.Count - 1) ? 0 : currentLevelIndex + 1;
-//        LoadLevel(nextLevelIndex);
-//    }
-
-//    void LoadHealPlayer(int amount)
-//    {
-//        LoadCanvas.SetActive(false);
-//        progressSoulAmount = 0;
-//        progressSoulSlider.value = 0;
-//    }
-
-//    void absorbSoul()
-//    {
-//        progressSoulAmount += 1;
-//        progressSoulSlider.value += 1;
-//    }
-//}
-using System;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -167,6 +64,31 @@ public class GameController : MonoBehaviour
         if (CheckpointManager.Instance != null)
             CheckpointManager.Instance.ResetCheckpoint();
         Time.timeScale = 1;
+    }
+
+    public void Respawn()
+    {
+        if (gameOverScreen != null)
+            gameOverScreen.SetActive(false);
+
+        Time.timeScale = 1f;
+
+        if (LoadCanvas != null) LoadCanvas.SetActive(false);
+        if (levels[currentLevelIndex] != null)
+        {
+            levels[currentLevelIndex].gameObject.SetActive(false);
+            levels[currentLevelIndex].gameObject.SetActive(true);
+        }
+
+        if (player != null)
+        {
+            Vector3 spawnPos = SpawnPoint != null ? SpawnPoint.position : Vector3.zero;
+            if (CheckpointManager.Instance != null)
+                spawnPos = CheckpointManager.Instance.GetRespawnPoint(spawnPos);
+            player.transform.position = spawnPos;
+        }
+
+        OnReset?.Invoke();
     }
 
     void IncreaseProgressSoulAmount(int amount)
