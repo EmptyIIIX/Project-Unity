b3fa1e3699e0dec498c777f8199b91e3184c6968
Thaizan666666 <Thaizan246810@gmail.com>
2026-03-16 20:06:40 +0700

    UpdateScene

---
+++ Assets/Script/MainManu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // โหลด Scene ด้วย Index
+    public void LoadScenePlay()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadSceneByPlay()
+    {
+        SceneManager.LoadScene("Main_Scene");
+    }
+}
